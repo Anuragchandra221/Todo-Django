db3c94a4ffa18a70d2fb9cbf49129b303780fae6
anurag <pothaluranurag2002@gmail.com>
2022-06-12 20:39:50 +0530

    filter and sort added

--- todos/views.py
+++ todos/views.py
@@ -2,6 +2,9 @@ from unicodedata import category
 from django.shortcuts import redirect, render
 from django.shortcuts import HttpResponse
 from .models import Category, TodoList
+from datetime import datetime
+
+from django.utils import timezone
 # Create your views here.
 
 def index(request):
@@ -23,7 +26,6 @@ def index(request):
 
         if "deleteTask" in request.POST:
             checked = request.POST.getlist("checkedbox")
-            print(checked)
             for id in checked:
                 todo = TodoList.objects.get(id=int(id))
                 todo.delete()
@@ -46,12 +48,20 @@ def index(request):
                 Cat.delete()
             return redirect(request.path)
 
-        
+        filter = request.POST['filter']
         sort = request.POST['sort']
         todolist = TodoList.objects.all().order_by(sort)
-        print(sort)
+        if filter=="All":
+            todolist = TodoList.objects.all()
+        else:
+            date_format = "%Y-%m-%d"
 
-    print(request.POST)
+            a = datetime.strptime(str(datetime.now().date()), date_format)
+            # b = datetime.strptime(str(todolist.due_date), date_format)
+            todolist = TodoList.objects.filter(due_date__lt = a)
+        
+        
+
 
     ctx = {
         "todos": todolist,
